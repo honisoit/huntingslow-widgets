eeba3a9c62787e04d932c68dbc7ae52d4e4f03f8
maxhall <maxwelljhall@gmail.com>
2016-07-10 14:31:11 +1000

    Initial Commit

---
+++ includes/widget-popular.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Adds Widget_Popular widget.
+ */
+class Widget_Popular extends WP_Widget {
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'Widget_Popular', // Base ID
+			__('Top 5 list', 'text_domain'), // Name
+			array( 'description' => 'Display top 5 posts from around the site') // Args
+		);
+	}
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+
+    echo $args['before_widget'];
+    ?>
+    <div class="popular">
+      <h1 class="popular__title">Most Popular</h1>
+      <ol class="popular__list">
+        <li class="popular__list-item">
+          <a href="#">Meat is bad</a>
+        </li>
+        <li class="popular__list-item">
+          <a href="#">Andrew is bad</a>
+        </li>
+        <li class="popular__list-item">
+          <a href="#">Bell is bad</a>
+        </li>
+        <li class="popular__list-item">
+          <a href="#">Yes is bad</a>
+        </li>
+        <li class="popular__list-item">
+          <a href="#">Everything is bad</a>
+        </li>
+      </ol>
+    </div>
+    <?php
+		echo $args['after_widget'];
+	}
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'text_domain' );
+		}
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php
+	}
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}
+} // class Widget_Popular
---
+++ widget-test.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: My Widget
+Plugin URI: http://mydomain.com
+Description: My first widget
+Author: Me
+Version: 1.0
+Author URI: http://mydomain.com
+*/
+// Block direct requests
+if ( !defined('ABSPATH') )
+	die('-1');
+
+//Get the absolute path of the directory that contains the file, with trailing slash.
+define('MY_PLUGIN_PATH', plugin_dir_path(__FILE__));
+//This is important, otherwise we'll get the path of a subdirectory
+require_once MY_PLUGIN_PATH . 'includes/widget-popular.php';
+require_once MY_PLUGIN_PATH . 'includes/widget-related.php';
+
+add_action( 'widgets_init', function(){
+     register_widget( 'Widget_Popular' );
+});
+
+
+add_action( 'widgets_init', function(){
+     register_widget( 'Widget_Related' );
+});
