c043216daeba4ccbf341950851c6a7b2ea87c7f9
maxhall <maxwelljhall@gmail.com>
2016-11-27 14:53:37 +1100

    Refactor aggregator-md

--- includes/aggregator-md.php
+++ includes/aggregator-md.php
@@ -32,109 +32,102 @@ class Huntingslow_Aggregator_Md extends WP_Widget {
   $query_term = $instance['query_term'];
   $override_feature_URL = $instance['override_feature_URL'];
 
-  // do a little business thinking
+  // todo: disable this hardcode and allow the featured articles to be overwritten
   $feature_is_overriden = '0';
   $override_feature_ID = '6';
 
   // spit that initial markup
-	echo $args['before_widget'];
-  echo '<div class="aggregator-md"><h1 class="aggregator-md__title">'. $title . '</h1>';
-	echo '<div class="flex-container">';
-	echo '<div class="aggregator-md__featured">';
+	echo $args['before_widget']; ?>
+  <div class="aggregator-md">
+		<h1 class="aggregator-md__title"><?php echo esc_html( $title ); ?></h1>
+		<div class="flex-container">
+			<div class="aggregator-md__featured">
+			  <?php // do the feature query
+			  $override_feature_args = array( 'p' => $override_feature_ID );
+			  $category__feature_args = array( 'posts_per_page' => 1, 'category_name' => $query_term );
+			  $tag_feature_args = array( 'posts_per_page' => 1, 'tag' => $query_term );
 
-  // do the feature query
-  $override_feature_args = array( 'p' => $override_feature_ID );
-  $category__feature_args = array( 'posts_per_page' => 1, 'category_name' => $query_term );
-  $tag_feature_args = array( 'posts_per_page' => 1, 'tag' => $query_term );
+			  if ( $feature_is_overriden == '1' ) {
+			    $feature_post = new WP_Query( $override_feature_args );
+			  } elseif ( $query_type = 'category') {
+			    $feature_post = new WP_Query( $category__feature_args );
+			  } else {
+			    $feature_post = new WP_Query( $tag_feature_args );
+			  };
 
-  if ( $feature_is_overriden == '1' ) {
-    $feature_post = new WP_Query( $override_feature_args );
-  } elseif ( $query_type = 'category') {
-    $feature_post = new WP_Query( $category__feature_args );
-  } else {
-    $feature_post = new WP_Query( $tag_feature_args );
-  };
+			  if ( $feature_post->have_posts() ) {
+				  while ( $feature_post->have_posts() ) {
+					  $feature_post->the_post(); ?>
+							<figure class="aggregator-md__featured-image">
+								<a href="<?php echo get_the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+							</figure>
+							<h4 class="aggregator-md__featured-headline">
+								<a href="<?php echo get_the_permalink() ?>"><?php echo get_the_title(); ?></a>
+							</h4>
+			      	<p class="aggregator-md__featured-standfirst">
+								<?php $standfirst = get_post_meta( get_the_id(), 'standfirst', true);
+								echo esc_html( $standfirst ) ?>
+							</p>
+							<p class="single-lg__byline aggregator-md__featured-byline">By <?php get_the_byline_link(); ?></p>
+						<?php }
+				  	/* Restore original Post Data */
+				  	wp_reset_postdata();
+			  } else {
+				  echo 'Something went wrong with the query.';
+			  } ?>
+			</div>
+			<div class="aggregator-md__list">
+			  <?php // now for the list query
+			  $override_category_list_args = array(
+			    'posts_per_page' => 5,
+			    'category_name' => $query_term,
+			    'post__not_in' => array( $override_feature_ID )
+			  );
+			  $override_tag_list_args = array(
+			    'posts_per_page' => 5,
+			    'tag' => $query_term,
+			    'post__not_in' => array( $override_feature_ID )
+			  );
+			  $category_list_args = array(
+			    'posts_per_page' => 5,
+			    'offset' => 1,
+			    'category_name' => $query_term
+			  );
+			  $tag_list_args = array(
+			    'posts_per_page' => 5,
+			    'offset' => 1,
+			    'tag' => $query_term
+			  );
 
-  if ( $feature_post->have_posts() ) {
-	  while ( $feature_post->have_posts() ) {
-		  $feature_post->the_post();
-      // get the thumbnail
-			echo '<figure class="aggregator-md__featured-image"><a href="';
-			echo get_the_permalink();
-			echo '">';
-			the_post_thumbnail();
-			echo '</a></figure>';
-			echo '<h4 class="aggregator-md__featured-headline"><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h4>';
-      echo '<p class="aggregator-md__featured-standfirst">' . get_post_meta( get_the_id(), 'standfirst', true) . '</p>';
-			echo '<p class="single-lg__byline aggregator-md__featured-byline">By ';
-			if ( function_exists( 'coauthors_posts_links' ) ) {
-				coauthors_posts_links();
-			} else {
-				the_author_posts_link();
-			}
-			echo '</p>';
-		}
-	  /* Restore original Post Data */
-	  wp_reset_postdata();
-  } else {
-	  echo 'Something went wrong with the query.';
-  }
+			  if ( $feature_is_overriden == '1' ) {
+			    if ( $query_type == 'category' ) {
+			      $list_posts = new WP_Query( $override_category_list_args );
+			    } else {
+			      $list_posts = new WP_Query( $override_tag_list_args );
+			    }
+			  } elseif ( $query_type == 'category' ) {
+			    $list_posts = new WP_Query( $category_list_args );
+			  } else {
+			    $list_posts = new WP_Query( $tag_list_args );
+			  }
 
-  echo '</div><div class="aggregator-md__list">';
+			  if ( $list_posts->have_posts() ) {
+			    while ( $list_posts->have_posts() ) {
+			      $list_posts->the_post(); ?>
+							<p class="aggregator-md__primary-tag"><?php echo get_the_primary_tag_link(); ?></p>
+			      	<p class="aggregator-md__headline"><a href="<?php echo get_the_permalink() ?>"><?php echo get_the_title(); ?></a></p>
+			    <?php }
+			    /* Restore original Post Data */
+			    wp_reset_postdata();
+			  } else {
+			    echo 'You made an error with the query. Try fixing the name of the category or tag you are trying to show';
+			  } ?>
+		  </div>
+		</div>
+	</div>
+  <?php echo $args['after_widget'];
+	}
 
-  // now for the list query
-  $override_category_list_args = array(
-    'posts_per_page' => 5,
-    'category_name' => $query_term,
-    'post__not_in' => array( $override_feature_ID )
-  );
-  $override_tag_list_args = array(
-    'posts_per_page' => 5,
-    'tag' => $query_term,
-    'post__not_in' => array( $override_feature_ID )
-  );
-  $category_list_args = array(
-    'posts_per_page' => 5,
-    'offset' => 1,
-    'category_name' => $query_term
-  );
-  $tag_list_args = array(
-    'posts_per_page' => 5,
-    'offset' => 1,
-    'tag' => $query_term
-  );
-
-  if ( $feature_is_overriden == '1' ) {
-    if ( $query_type == 'category' ) {
-      $list_posts = new WP_Query( $override_category_list_args );
-    } else {
-      $list_posts = new WP_Query( $override_tag_list_args );
-    }
-  } elseif ( $query_type == 'category' ) {
-    $list_posts = new WP_Query( $category_list_args );
-  } else {
-    $list_posts = new WP_Query( $tag_list_args );
-  }
-
-  if ( $list_posts->have_posts() ) {
-    while ( $list_posts->have_posts() ) {
-      $list_posts->the_post();
-      // get the thumbnail
-			$primary_tag_id = get_post_meta( get_the_id(), 'primary_tag', true );
-			$primary_tag_array = get_term_by( 'id', $primary_tag_id, 'post_tag', ARRAY_A);
-			$primary_tag = ucwords($primary_tag_array['name']);
-			echo '<p class="aggregator-md__primary-tag"><a href="">' . $primary_tag . '</a></p>';
-      echo '<p class="aggregator-md__headline"><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></p>';
-    }
-    /* Restore original Post Data */
-    wp_reset_postdata();
-  } else {
-    echo 'You made an error with the query. Try fixing the name of the category or tag you are trying to show';
-  }
-  echo '</div></div></div>';
-  echo $args['after_widget']; ?>
-
-	<?php }
 	/**
 	 * Back-end widget form.
 	 *
@@ -175,7 +168,7 @@ class Huntingslow_Aggregator_Md extends WP_Widget {
 
 		<hr>
 
-		<p>Optional:</p>
+		<p>Optional: (Don't use yet, soz)</p>
     <p>
     <label for="<?php echo esc_attr( $this->get_field_id( 'override_feature_URL' ) ); ?>"><?php _e( 'Feature override URL', 'wp_widget_plugin' ); ?></label>
     <input id="<?php echo esc_attr( $this->get_field_id( 'override_feature_URL' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'override_feature_URL' ) ); ?>" type="text" value="<?php echo esc_attr( $override_feature_URL ); ?>" />
